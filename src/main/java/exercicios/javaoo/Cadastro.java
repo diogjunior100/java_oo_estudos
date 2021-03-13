f038169e3ed1fd58ddc21ca51018b5a0925b0e84
Diógenes Júnior <diogjunior10071@gmail.com>
2021-03-13 16:42:25 -0300

    Alterando Cadastro

---
+++ src/main/java/exercicios/javaoo/Cadastro.java
@@ -0,0 +1,33 @@
+
+package exercicios.javaoo;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Cadastro {
+    int ano,
+        idade;
+    String nome;
+
+    Cadastro() {
+        
+    }
+    
+    Scanner sc = new Scanner(System.in);
+    
+    void cadastrarDados(){
+	ano = sc.nextInt();
+        nome = sc.next();
+        
+    }
+
+    void imprimirDados() {
+        System.out.println("Esse e o seu nome " + nome);
+        
+        idade = 2021 - ano;
+        System.out.println("Esse e o sua idade " + idade);
+    }
+    
+        
+    
+}
